1c9491b7bfe8cb46421eca92606971b7b6ff7a94
Oleksii <burmistrenkoleksii@gmail.com>
2022-11-02 22:17:13 +0200

    Initial commit

---
+++ src/main/java/Bowling.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Bowling {
+
+
+    HashMap<String, Integer> players = new HashMap<String, Integer>();
+
+    Bowling() {
+
+
+
+        players.put("Victor", 57);
+        players.put("John", 56);
+        players.put("Amy", 100);
+        players.put("Jerry", 101);
+        players.put("Bob", 89);
+    }
+
+    //your code goes here
+
+    // Alternative solution
+    public void getWinner() {
+        Map.Entry<String,Integer> maxEntry =null;
+        for (Map.Entry<String,Integer> entry: players.entrySet()){
+            if( maxEntry==null || entry.getValue() > maxEntry.getValue()){
+                maxEntry =entry;
+            }
+
+        }
+        System.out.println(maxEntry.getKey());
+
+
+
+
+
+
+    }
+}
